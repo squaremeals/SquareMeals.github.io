08bac9b7018089bf7908b4322e9fab364916660e
Francesca Bueti <fbueti@gmail.com>
2017-10-24 15:13:05 -0400

    updated design

--- models/EmailSignup.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var schema = mongoose.Schema({
-	emailAddress: String,
-});
-
-var EmailSignup = mongoose.model('EmailSignup', schema);
-module.exports = EmailSignup;
